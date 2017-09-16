c961d38b3f749a9c3f127f1b4ade31f73704172d
jairoadi <jairoadi@gmail.com>
2017-09-16 01:34:46 -0600

    works minimally

--- arrays/array_api.py
+++ arrays/array_api.py
@@ -18,6 +18,7 @@ class Array(object):
         
     def debug_print(self):
         '''Prints a representation of the entire allocated space, including unused spots.'''
+        print('DEBUG')
         print(self.size , 'of' , len(self.data) , '>>>' , self.data)
         
         
@@ -28,10 +29,6 @@ class Array(object):
     def _check_increase(self):
         '''Checks whether the array is full and needs to increase by chunk size in preparation for adding an item to
         the array.'''
-
-        for index, slot in enumerate(self.data):
-            if slot == None:
-                break
 
         if self.size == len(self.data):
             'increment by 5'
@@ -48,20 +45,20 @@ class Array(object):
 
         self.tempData = []  # creates a tempData arrayObject
 
-        while len(self.data) - self.size > 5:
-
+        while self.countNone >= 5:
 
             for i in range(len(self.data)-5):
                 self.tempData.append(self.data[i]) #assigning the data values to the tempData array
 
             self.data = self.tempData #creating a new data array with smaller length
+            self.check_size()
 
         del self.tempData #deleting tempData to not have unnecessary memory allocation being used
 
         
     def add(self, item):
         '''Adds an item to the end of the array, allocating a larger array if necessary.'''
-        self._check_increase()
+        self.check_size()
         self.data[self.size] = item
         self.size += 1
 
@@ -70,37 +67,39 @@ class Array(object):
 
     def insert(self, index, item):
         '''Inserts an item at the given index, shifting remaining items right and allocating a larger array if necessary.'''
-        countNone = 0
+        print('INSERT', index,',',item)
+        self.check_size()
+
         iCount = 0
         self.tempData = []
 
-        for i in self.data:
-            if i is None:
-                countNone += 1
+        if index > self.size:
+            print('Error:', index, 'is not within the bounds of the current array.')
+            return
 
-        if countNone is 0:
-            self._check_increase()
+        if self.size < len(self.data):
+            while iCount < int(index):
+                self.tempData.append(self.data[iCount])
+                iCount += 1
 
+            self.tempData.append(item)
 
+            while iCount < len(self.data)- 1:
 
-        while iCount < int(index):
-            self.tempData.append(self.data[iCount])
-            iCount += 1
+                self.tempData.append(self.data[iCount])
 
-        self.tempData.append(item)
+                iCount += 1
 
-        while iCount < len(self.data)- 1:
-            self.tempData.append(self.data[iCount])
+            self.data = self.tempData
+            del self.tempData
 
-            iCount += 1
-
-        self.data = self.tempData
-
-        del self.tempData
+            self.check_size()
 
     
     def set(self, index, item):
         '''Sets the given item at the given index. Throws an exception if the index is not within the bounds of the array.'''
+        print('SET', index, ',', item)
+
         inRange = False
 
         for i in range(self.size):
@@ -110,41 +109,76 @@ class Array(object):
 
         if not inRange:
             print('Error:', index, 'is not within the bounds of the current array.')
-        else:
-            print('SET', index, item)
+
 
         
     def get(self, index):
         '''Retrieves the item at the given index.  Throws an exception if the index is not within the bounds of the array.'''
         inRange = False
         getValue = None
+        print('GET', index)
 
         for i in range(self.size):
             if i == index:
                 getValue = self.data[i]
                 inRange = True
+                break
 
         if not inRange:
             print('Error:', index, 'is not within the bounds of the current array.')
         else:
-            print('GET', getValue)
+            print(getValue)
+
     
     def delete(self, index):
         '''Deletes the item at the given index, decreasing the allocated memory if needed.  Throws an exception if the index is not within the bounds of the array.'''
-        # for i in range(10):
-        #     self.data.append(None)
+        print('DELETE',index)
+        iCount = 0
+        self.tempData = []
+
+        if index > self.size:
+            print('Error:', index, 'is not within the bounds of the current array.')
+            return
 
         for i in range(len(self.data)):
             if i == index:
                 self.data[i] = None
                 break
+            iCount += 1
 
+        for i,value in enumerate(self.data):
+            if value != None:
+                self.tempData.append(self.data[i])
+
+        while len(self.tempData) < len(self.data):
+            self.tempData.append(None)
+
+
+        self.data = self.tempData
+        del self.tempData
+
+
+        self.check_size()
         self._check_decrease()
+
         
     def swap(self, index1, index2):
         '''Swaps the values at the given indices.'''
-        
-        
+
+    def check_size(self):
+        '''checks is there are spaces in the array'''
+        # check size
+        self.countNone = 0
+        self.size = 0
+        for i in range(len(self.data)):
+            if self.data[i] != None:
+                self.size += 1
+            else:
+                self.countNone += 1
+
+
+        if self.size == len(self.data):
+            self._check_increase()
         
         
 ###################################################
@@ -154,7 +188,6 @@ def alloc(size):
     '''
     Allocates array space in memory. This is similar to C's alloc function.
     '''
-    
 
 def memcpy(dest, source, size):
     '''
